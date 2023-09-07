df3c7256e1196f282b18f60ae308a3447cf8ce6d
Johann Felipe Rojas <johannrip@gmail.com>
2023-09-06 22:23:38 -0500

    style: remove container

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,15 @@ export default function App() {
   useAccountEffect();
 
   return (
-    <div className="container py-3">
-      <div className="row">
-        <div className="col-12 order-1">
-          <SelectedAccountName />
-        </div>
-        <div className="col-12 col-lg-5 col-xl-4 order-2 mb-3 mb-lg-0">
-          <Accounts />
-        </div>
-        <div className="col-12 col-lg-7 col-xl-8 h-100 order-3">
-          <ActivityContainer />
-        </div>
+    <div className="row">
+      <div className="col-12 order-1">
+        <SelectedAccountName />
+      </div>
+      <div className="col-12 col-lg-5 col-xl-4 order-2 mb-3 mb-lg-0">
+        <Accounts />
+      </div>
+      <div className="col-12 col-lg-7 col-xl-8 h-100 order-3">
+        <ActivityContainer />
       </div>
     </div>
   );
