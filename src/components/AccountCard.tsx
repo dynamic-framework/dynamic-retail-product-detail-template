516566ec2e17ff876138250fadec3630c924daad
Eduard Baron <ebaron@modyo.com>
2024-05-08 17:47:00 -0500

    chore: adjust account card design

--- src/components/AccountCard.tsx
+++ src/components/AccountCard.tsx
@@ -1,6 +1,8 @@
 import classNames from 'classnames';
-import { useTranslation } from 'react-i18next';
 
+import { ReactComponent as Chip } from '../assets/Chip.svg';
+import { ReactComponent as DynamicLogo } from '../assets/Dynamic.svg';
+import { ReactComponent as MastercardLogo } from '../assets/Mastercard.svg';
 import { API_ACCOUNT_LIST_FILTER } from '../config/widgetConfig';
 import { BaseAccount } from '../services/interface';
 
@@ -9,49 +11,27 @@ type Props = {
 };
 
 export default function AccountCard({ account }: Props) {
-  const { t } = useTranslation();
   return (
     <div
-      className={classNames('account-card', API_ACCOUNT_LIST_FILTER)}
-    >
-      {API_ACCOUNT_LIST_FILTER === 'credit-card' && (
-        <div className="account-card-icons">
-          <img
-            src="https://cloud.modyocdn.com/uploads/389459e5-895d-485e-bffe-855e0eef0869/original/Chip.png"
-            alt="Chip"
-            width={38}
-          />
-          <img
-            src="https://cloud.modyocdn.com/uploads/b3f99e99-8322-4f1f-aefc-c6c5de86b764/original/Mastercard.png"
-            alt="Mastercard"
-            width={40}
-          />
-        </div>
+      className={classNames(
+        'account-card',
+        API_ACCOUNT_LIST_FILTER,
       )}
-      <div className="account-card-number">
-        {account.accountNumber}
+    >
+      <div className="d-flex justify-content-between align-items-start">
+        <DynamicLogo height={18} width={80} />
+        <Chip height={22} width={32} />
       </div>
-      <div className="account-card-details">
+      <div className="d-flex justify-content-between align-items-end">
         <div className="d-flex flex-column flex-grow-1">
-          {API_ACCOUNT_LIST_FILTER === 'credit-card' && (
-            <small>
-              {t('card.name')}
-            </small>
-          )}
-          <span className="name">
+          <small className="name">
             {account.name}
-          </span>
-        </div>
-        {API_ACCOUNT_LIST_FILTER === 'credit-card' && (
-          <div className="d-flex flex-column">
-            <small>
-              {t('card.date')}
-            </small>
-            <span className="date">
-              01/22
-            </span>
+          </small>
+          <div className="account-card-number">
+            {account.accountNumber}
           </div>
-        )}
+        </div>
+        <MastercardLogo height={32} width={44} />
       </div>
     </div>
   );
