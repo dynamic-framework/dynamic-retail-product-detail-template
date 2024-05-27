5cd1b46579fe56330e00ce425741c4584915077e
lnazarit <lnazarit@gmail.com>
2024-05-27 13:34:25 -0400

    feat: implement cardItem component and responsive modal cardinformation component

--- src/components/modals/ModalCardInformation.tsx
+++ src/components/modals/ModalCardInformation.tsx
@@ -1,4 +1,5 @@
 import {
+  DButton,
   DIcon,
   DModal,
   useDPortalContext,
@@ -9,7 +10,7 @@ import useCVC from '../../hooks/useCVC';
 import { Account } from '../../services/interface';
 import { useAppSelector } from '../../store/hooks';
 import { getAccountSelected } from '../../store/selectors';
-import AccountCard from '../AccountCard';
+import CardItem from '../CardItem';
 import ItemCardInfo from '../ItemCardInfo';
 import CircularLoaderWidthTimer from '../loaders/CircularLoaderWithTimer';
 
@@ -18,58 +19,62 @@ const DURATION = 20;
 export default function ModalCardInformation() {
   const { t } = useTranslation();
   const { closePortal } = useDPortalContext();
-
   const account = useAppSelector(getAccountSelected) as Account;
-
   const cvc = useCVC(DURATION);
 
+  const numberInfo = (cardNumber: string) => (
+    <div className="d-flex w-100 align-items-center">
+      <span className="flex-1">{cardNumber}</span>
+      <DButton
+        onClick={() => navigator.clipboard.writeText(account.accountNumber)}
+        className="ml-auto"
+        text="Copy"
+        theme="secondary"
+        iconStart="copy"
+        variant="link"
+      />
+    </div>
+  );
+
   return (
-    <DModal
-      name="modalOTP"
-      centered
-      staticBackdrop
-    >
-      <DModal.Header
-        onClose={closePortal}
-        showCloseButton
-      >
+    <DModal name="modalOTP" centered staticBackdrop size="lg">
+      <DModal.Header onClose={closePortal} showCloseButton>
         <h5 className="fw-bold">{t('cardInfo.title')}</h5>
       </DModal.Header>
       <DModal.Body className="p-8">
-        <div className="mb-8 rounded bg-gray-100 p-10">
-          <AccountCard account={account} />
-          <div className="text-center mt-2">
-            <small className="d-inline-flex gap-2 align-items-center">
-              <DIcon
-                icon="circle-fill"
-                theme="success"
-                size="8px"
-              />
-              <span>Active</span>
-            </small>
+        <div className="d-block d-lg-flex gap-4 align-items-start">
+          <div className="mb-8 rounded bg-gray-100 p-10 order-1">
+            <CardItem account={account} />
+            <div className="text-center mt-2">
+              <small className="d-inline-flex gap-2 align-items-center">
+                <DIcon icon="circle-fill" theme="success" size="8px" />
+                <span>Active</span>
+              </small>
+            </div>
           </div>
-        </div>
-        <ItemCardInfo
-          name={t('cardInfo.cardHolderName')}
-          value={account?.alias}
-        />
-        <ItemCardInfo
-          name={t('cardInfo.cardNumber')}
-          value={account?.accountNumber}
-        />
-        <ItemCardInfo
-          name={t('cardInfo.validUntil')}
-          value={account?.accountNumber}
-        />
-        <ItemCardInfo
-          name={t('cardInfo.cvc')}
-          value={cvc.cvc}
-        />
-        <div className="d-flex gap-2">
-          <CircularLoaderWidthTimer size={20} duration={DURATION} />
-          <p className="text-gray-500">
-            {`${t('cardInfo.cvcWillChange')} ${cvc.secondsLeft} ${t('cardInfo.seconds')}`}
-          </p>
+          <div className="order-0 flex-1">
+            <ItemCardInfo
+              name={t('cardInfo.cardHolderName')}
+              value={account?.alias}
+            />
+            <ItemCardInfo
+              name={t('cardInfo.cardNumber')}
+              value={numberInfo(account.accountNumber)}
+            />
+            <ItemCardInfo
+              name={t('cardInfo.validUntil')}
+              value={account?.accountNumber}
+            />
+            <ItemCardInfo name={t('cardInfo.cvc')} value={cvc.cvc} />
+            <div className="d-flex gap-2">
+              <CircularLoaderWidthTimer size={20} duration={DURATION} />
+              <p className="text-gray-500">
+                {`${t('cardInfo.cvcWillChange')} ${cvc.secondsLeft} ${t(
+                  'cardInfo.seconds',
+                )}`}
+              </p>
+            </div>
+          </div>
         </div>
       </DModal.Body>
     </DModal>
