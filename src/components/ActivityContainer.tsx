43c53b7f5928d4ddb5851e30baca1b58f11698d6
Eduard Baron <42986303+efbarong@users.noreply.github.com>
2024-10-09 16:52:15 -0500

    chore: initial changes for new tabs (#37)
    * chore: initial changes for new tabs
    
    * chore: remove components

--- src/components/ActivityContainer.tsx
+++ src/components/ActivityContainer.tsx
@@ -1,13 +1,21 @@
-import { DTabContent, DTabs, DTabOption } from '@dynamic-framework/ui-react';
+import {
+  DTabContent,
+  DTabs,
+  DTabOption,
+  DCard,
+} from '@dynamic-framework/ui-react';
 import { useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import { API_ACCOUNT_LIST_FILTER } from '../config/widgetConfig';
 import { useAppSelector } from '../store/hooks';
 import { getIsNotReady } from '../store/selectors';
 
 import ActivityList from './ActivityList';
 import ActivityListScheduled from './ActivityListScheduled';
 import AccountListLoader from './loaders/AccountListLoader';
+
+const isCreditCard = API_ACCOUNT_LIST_FILTER === 'credit-card';
 
 export default function ActivityContainer() {
   const { t } = useTranslation();
@@ -16,7 +24,16 @@ export default function ActivityContainer() {
   const options: DTabOption[] = useMemo(() => [
     { label: t('tabs.transactions'), tab: 'transactions' },
     { label: t('tabs.upcoming'), tab: 'upcoming' },
+    { label: t('tabs.complains'), tab: 'complains' },
+    { label: t('tabs.newTab'), tab: 'newTab' },
   ], [t]);
+
+  const filteredOptions = useMemo(() => {
+    if (!isCreditCard) {
+      return options.filter(({ tab }) => tab !== 'complains');
+    }
+    return options;
+  }, [options]);
 
   const [container, setContainer] = useState(options[0]);
 
@@ -34,19 +51,28 @@ export default function ActivityContainer() {
   }
 
   return (
-    <div className="d-block py-0 px-4 w-100 bg-white rounded">
-      <DTabs
-        options={options}
-        defaultSelected={container.tab}
-        onChange={handlerSelected}
-      >
-        <DTabContent tab={options[0].tab}>
-          <ActivityList />
-        </DTabContent>
-        <DTabContent tab={options[1].tab}>
-          <ActivityListScheduled />
-        </DTabContent>
-      </DTabs>
-    </div>
+    <DCard>
+      <DCard.Body>
+        <DTabs
+          options={filteredOptions}
+          defaultSelected={container.tab}
+          onChange={handlerSelected}
+          className="px-0 pt-0"
+        >
+          <DTabContent tab={options[0].tab}>
+            <ActivityList />
+          </DTabContent>
+          <DTabContent tab={options[1].tab}>
+            <ActivityListScheduled />
+          </DTabContent>
+          <DTabContent tab={options[2].tab}>
+            <p>Complains</p>
+          </DTabContent>
+          <DTabContent tab={options[3].tab}>
+            <p>New tab</p>
+          </DTabContent>
+        </DTabs>
+      </DCard.Body>
+    </DCard>
   );
 }
