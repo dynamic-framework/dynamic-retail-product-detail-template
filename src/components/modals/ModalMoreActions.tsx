ee1bc515dcb884612c7c14aa10e72568c6de165e
pipemeyer <fmeyer@modyo.com>
2024-11-18 11:48:35 -0300

    fix: resolve comments

--- src/components/modals/ModalMoreActions.tsx
+++ src/components/modals/ModalMoreActions.tsx
@@ -1,20 +1,8 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import {
-  DModal,
-  useDPortalContext,
-  DIcon,
-} from '@dynamic-framework/ui-react';
+import { DModal, useDPortalContext, DIcon } from '@dynamic-framework/ui-react';
 import { useTranslation } from 'react-i18next';
 
-import {
-  BANK_STATEMENTS_PATH,
-  CASH_ADVANCE_PATH,
-  CONFIGURE_RESTRICTIONS_PATH,
-  ADDITIONAL_CARDS_PATH,
-  NEW_ADDITIONAL_CARDS_PATH,
-  NEW_VIRTUAL_CARD_PATH,
-  SITE_URL,
-} from '../../config/widgetConfig';
+import { SITE_URL } from '../../config/widgetConfig';
+import { useMoreActions } from '../../hooks/useMoreActions';
 import { useAppSelector } from '../../store/hooks';
 import { getAccountSelected } from '../../store/selectors';
 
@@ -22,48 +10,9 @@ export default function ModalMoreActions() {
   const { closePortal } = useDPortalContext();
   const { t } = useTranslation();
   const account = useAppSelector(getAccountSelected)!;
-  const MORE_ACTIONS = [
-    {
-      text: 'modal.moreActions.bankStatements',
-      link: BANK_STATEMENTS_PATH,
-      icon: 'file-text',
-    },
-    {
-      text: 'modal.moreActions.cashAdvance',
-      link: CASH_ADVANCE_PATH,
-      icon: 'cash-coin',
-    },
-    {
-      text: 'modal.moreActions.additionalCardsList',
-      link: `${ADDITIONAL_CARDS_PATH}?card_id=${account.id}`,
-      icon: 'credit-card-2-back',
-    },
-    {
-      text: 'modal.moreActions.newAdditionalCard',
-      link: `${NEW_ADDITIONAL_CARDS_PATH}?card_id=${account.id}`,
-      icon: 'plus-circle',
-    },
-    {
-      text: 'modal.moreActions.newVirtualCard',
-      link: `${NEW_VIRTUAL_CARD_PATH}?card_id=${account.id}`,
-      icon: 'phone',
-    },
-    {
-      text: 'modal.moreActions.configureRestrictions',
-      link: CONFIGURE_RESTRICTIONS_PATH,
-      icon: 'toggles',
-    },
-    {
-      text: 'modal.moreActions.managePayments',
-      link: '#',
-      icon: 'receipt',
-    },
-    {
-      text: 'modal.moreActions.changeDate',
-      link: '#',
-      icon: 'calendar-date',
-    },
-  ];
+
+  const getMoreActions = useMoreActions();
+  const moreActions = getMoreActions(account.id);
 
   return (
     <DModal
@@ -78,7 +27,7 @@ export default function ModalMoreActions() {
       </DModal.Header>
       <DModal.Body>
         <div className="d-flex flex-column gap-4">
-          {MORE_ACTIONS.map((action) => (
+          {moreActions.map((action) => (
             <a
               key={action.text}
               className="link link-primary d-inline-flex gap-2 text-decoration-none py-2"
