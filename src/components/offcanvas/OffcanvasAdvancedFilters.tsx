83effe76f541d5262eb8b22027b31851b34ed446
Eduard Baron <ebaron@modyo.com>
2025-01-13 12:28:23 -0500

    fix: filters design

--- src/components/offcanvas/OffcanvasAdvancedFilters.tsx
+++ src/components/offcanvas/OffcanvasAdvancedFilters.tsx
@@ -1,117 +1,115 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import {
   DButton,
   DDatePicker,
-  DIcon,
-  DInputCheck,
   DInputCurrency,
   DOffcanvas,
-  DOffcanvasBody,
-  DOffcanvasFooter,
-  DOffcanvasHeader,
   useDPortalContext,
 } from '@dynamic-framework/ui-react';
-import { DateTime } from 'luxon';
-import { useCallback, useState } from 'react';
+import {
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
 import { useTranslation } from 'react-i18next';
 
-export default function OffcanvasAdvancedFilters() {
-  const { t } = useTranslation();
-  const { closePortal } = useDPortalContext();
-  const [startDate, setStartDate] = useState<string | null>(null);
-  const [endDate, setEndDate] = useState<string | null>(null);
-  const [fromAmount, setFromAmount] = useState<number | undefined>(0);
-  const [toAmount, setToAmount] = useState<number | undefined>(0);
+import { useAppDispatch } from '../../store/hooks';
+import { setQueryFilterActivities } from '../../store/slice';
 
-  const onChangeDate = useCallback((value: Date | [Date | null, Date | null] | null) => {
-    const [newStartDate, newEndDate] = value as Array<Date>;
-    setStartDate(DateTime.fromJSDate(newStartDate).toISODate());
-    setEndDate(DateTime.fromJSDate(newEndDate).toISODate());
-  }, []);
+export default function FiltersOffcanvas() {
+  const { closePortal } = useDPortalContext();
+  const { t } = useTranslation();
+  const dispatch = useAppDispatch();
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(startDate);
+
+  const [minAmount, setMinAmount] = useState(0);
+  const [maxAmount, setMaxAmount] = useState(minAmount);
+
+  const filterHandler = useCallback(() => {
+    dispatch(setQueryFilterActivities(' '));
+    closePortal();
+  }, [closePortal, dispatch]);
+
+  const clearFilterHandler = useCallback(() => {
+    dispatch(setQueryFilterActivities(''));
+    closePortal();
+  }, [closePortal, dispatch]);
+
+  useEffect(() => {
+    if (endDate < startDate) {
+      setEndDate(startDate);
+    }
+  }, [endDate, startDate]);
 
   return (
     <DOffcanvas
-      name="advancedFilters"
-      openFrom="end"
+      className="filters-offcanvas"
+      name="filtersOffcanvas"
       staticBackdrop
     >
-      <DOffcanvasHeader
-        onClose={closePortal}
+      <DOffcanvas.Header
         showCloseButton
+        onClose={closePortal}
       >
-        <div className="d-flex align-items-center gap-2">
-          <DIcon
-            icon="filter"
-            size="24px"
-            className="p-2"
-          />
-          <h5 className="fw-bold">{t('filter.title')}</h5>
+        <h4>{t('filter.title')}</h4>
+      </DOffcanvas.Header>
+      <DOffcanvas.Body className="d-flex flex-column gap-6 py-6">
+        <div className="d-flex flex-column gap-4">
+          <h5 className="fw-normal">
+            {t('filters.dateRange')}
+          </h5>
+          <div className="d-flex flex-column flex-sm-row gap-4">
+            <DDatePicker
+              inputLabel={t('filters.startDate')}
+              startDate={startDate}
+              onChange={(e) => setStartDate(e!)}
+              date={startDate.toISOString()}
+            />
+            <DDatePicker
+              minDate={startDate}
+              inputLabel={t('filters.endDate')}
+              endDate={endDate}
+              onChange={(e) => setEndDate(e!)}
+              date={endDate.toISOString()}
+            />
+          </div>
         </div>
-      </DOffcanvasHeader>
-      <DOffcanvasBody>
-        <div className="d-flex flex-column gap-6 pt-4">
-          <DDatePicker<never, true>
-            onChange={onChangeDate}
-            placeholder={t('filters.dateRange')}
-            selectsRange
-            {...startDate && {
-              selected: DateTime.fromISO(startDate).toJSDate(),
-              startDate: DateTime.fromISO(startDate).toJSDate(),
-            }}
-            {...endDate && {
-              endDate: DateTime.fromISO(endDate).toJSDate(),
-            }}
-          />
-          <hr className="my-0" />
-          <p className="fw-bold small mb-0">
+
+        <hr className="m-0" />
+
+        <div className="d-flex flex-column gap-4">
+          <h5 className="fw-normal">
             {t('filters.amount')}
-          </p>
-          <div className="d-flex gap-6">
-            <DInputCurrency
-              id="from"
-              value={fromAmount}
-              label={t('filter.amountFrom')}
-              onChange={(newAmount) => setFromAmount(newAmount)}
-            />
-            <DInputCurrency
-              id="to"
-              value={toAmount}
-              label={t('filter.amountTo')}
-              onChange={(newAmount) => setToAmount(newAmount)}
-            />
-          </div>
-          <hr className="my-0" />
-          <div>
-            <p className="fw-bold small mb-4">{t('filters.type')}</p>
-            <div className="d-flex gap-4">
-              <DInputCheck
-                id="out"
-                name="tipo"
-                type="radio"
-                label={t('filter.typeCredit')}
-              />
-              <DInputCheck
-                id="in"
-                name="tipo"
-                type="radio"
-                label={t('filter.typeDebit')}
-              />
-            </div>
-          </div>
+          </h5>
+          <DInputCurrency
+            label={t('filters.minAmount')}
+            onChange={(e) => setMinAmount(e!)}
+            value={minAmount}
+          />
+          <DInputCurrency
+            label={t('filters.maxAmount')}
+            min={minAmount}
+            onChange={(e) => setMaxAmount(e!)}
+            value={maxAmount}
+            invalid={maxAmount < minAmount}
+          />
         </div>
-      </DOffcanvasBody>
-      <DOffcanvasFooter>
+      </DOffcanvas.Body>
+      <DOffcanvas.Footer
+        actionPlacement="fill"
+        className="flex-column-reverse flex-sm-row"
+      >
         <DButton
-          text={t('filters.cancel')}
+          text={t('button.cleanFilters')}
           variant="outline"
-          theme="secondary"
-          onClick={closePortal}
+          onClick={clearFilterHandler}
         />
         <DButton
-          text={t('filters.filter')}
-          onClick={closePortal}
+          text={t('button.apply')}
+          onClick={filterHandler}
         />
-      </DOffcanvasFooter>
+      </DOffcanvas.Footer>
     </DOffcanvas>
   );
 }
