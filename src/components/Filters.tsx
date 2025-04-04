ebc0fea447356339c582bcf2eaaed30bb8cb44bf
Eduard Baron <42986303+efbarong@users.noreply.github.com>
2025-04-03 22:47:19 -0500

    chore: remove button and label filter text (#51)
    * chore: remove button and label filter text
    
    * chore: update dependencies

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,8 +1,7 @@
 import {
-  DButton,
+  DButtonIcon,
   DInputSearch,
   useDPortalContext,
-  useMediaBreakpointUpMd,
 } from '@dynamic-framework/ui-react';
 import {
   ReactNode,
@@ -31,7 +30,6 @@ export default function Filters(
   const dispatch = useAppDispatch();
   const { t } = useTranslation();
   const { openPortal } = useDPortalContext();
-  const md = useMediaBreakpointUpMd(true);
 
   const [inputValue, setInputValue] = useState('');
 
@@ -42,29 +40,23 @@ export default function Filters(
   }, [debouncedQuery, dispatch]);
 
   return (
-    <>
-      <div className="d-flex d-lg-none align-items-center pb-2 ps-1">
-        <p className="text-gray-600 mb-0">{t('filters.filterBy')}</p>
-      </div>
-      <div className="d-flex align-items-center gap-6 mb-4">
-        <DInputSearch
-          id="inputSearch"
-          value={inputValue}
-          disabled={disabled}
-          placeholder={t('filters.search')}
-          onChange={(e) => setInputValue(e)}
-          className="col col-lg-4"
-        />
-        <DButton
-          variant="outline"
-          iconStart="funnel"
-          text={md ? t('filter.title') : ''}
-          className="px-4"
-          onClick={() => openPortal(offcanvasName, undefined)}
-          disabled={disabled}
-        />
-        {otherOptions}
-      </div>
-    </>
+    <div className="d-flex align-items-center gap-4 mb-4">
+      <DInputSearch
+        id="inputSearch"
+        value={inputValue}
+        disabled={disabled}
+        placeholder={t('filters.search')}
+        onChange={(e) => setInputValue(e)}
+        className="col col-lg-4"
+      />
+      <DButtonIcon
+        variant="outline"
+        icon="funnel"
+        onClick={() => openPortal(offcanvasName, {})}
+        disabled={disabled}
+        size="lg"
+      />
+      {otherOptions}
+    </div>
   );
 }
