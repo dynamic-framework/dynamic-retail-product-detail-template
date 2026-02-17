290c0dc8eb5a0ad0c2f91dfdd3621e3f9d65bd56
lnazarit <lnazarit@modyo.com>
2026-02-17 12:36:30 -0300

    feat: adjustement grid

--- src/components/ActionsSelectorSlider.tsx
+++ src/components/ActionsSelectorSlider.tsx
@@ -52,7 +52,7 @@ export default function ItemActions(
         />
       </div>
       <hr className="m-0 border-light" />
-      <div className="row row-cols-4 row-cols-lg-2 g-4">
+      <div className="row row-cols-1 row-cols-lg-2 g-4">
         <ActionsSelectorButton
           text={text}
           icon={icon}
